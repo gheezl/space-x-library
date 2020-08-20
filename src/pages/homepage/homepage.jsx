f06ba2e5ff22ca09fef280a424eaa40a76302f36
gheezl <gheezl25@gmail.com>
2020-08-19 21:04:29 -0400

    Adding a hover feature over the homepage links

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,9 +1,13 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link, Route } from "react-router-dom"
 
 import './homepage.css';
 
 const HomePage = () => {
+    let [toggleRockets, setToggleRockets] = useState(false)
+    let [toggleMissions, setToggleMissions] = useState(false)
+    let [toggleLaunchPads, setToggleLaunchPads] = useState(false)
+
     return (
         <Fragment>
             <h2 className="all-information">
@@ -12,18 +16,70 @@ const HomePage = () => {
             <div className="links">
                 <div className="link">
                     <Link to="/rockets" >
-                        <span>Rockets ☷</span>
+                        <span
+                            onMouseEnter={() => setToggleRockets(!toggleRockets)}
+                            onMouseLeave={() => setToggleRockets(!toggleRockets)}
+                        >
+                            Rockets ☷
+                        </span>
                     </Link>
+                    {
+                        toggleRockets
+                            ? (
+                                <div className="all-rockets">
+                                    <span>
+                                        All of the Space X rockets
+                                    </span>
+                                </div>
+                            )
+                            : (
+                                null
+                            )
+                    }
                 </div>
                 <div className="link">
                     <Link to="/missions" >
-                        <span>Missions ☷</span>
+                        <span
+                            onMouseEnter={() => setToggleMissions(!toggleMissions)}
+                            onMouseLeave={() => setToggleMissions(!toggleMissions)}
+                        >
+                            Missions ☷
+                        </span>
                     </Link>
+                    {
+                        toggleMissions
+                            ? (
+                                <div className="all-rockets">
+                                    <span>
+                                        All of Space X's missions
+                                    </span>
+                                </div>
+                            )
+                            : (
+                                null
+                            )
+                    }
                 </div>
                 <div className="link">
                     <Link to="/launch-pads" >
-                        <span>Launch Pads ☷</span>
+                        <span
+                            onMouseEnter={() => setToggleLaunchPads(!toggleLaunchPads)}
+                            onMouseLeave={() => setToggleLaunchPads(!toggleLaunchPads)}
+                        >Launch Pads ☷</span>
                     </Link>
+                    {
+                        toggleLaunchPads
+                            ? (
+                                <div className="all-rockets">
+                                    <span>
+                                        All of the Space X launch pads
+                                    </span>
+                                </div>
+                            )
+                            : (
+                                null
+                            )
+                    }
                 </div>
                 <div className="link">
                     <Link to="/launches">
